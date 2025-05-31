ac0670ddebe3490e36aac7768ae67cab35c540ed
Micael Macedo <micaelm2009@hotmail.com>
2025-05-31 17:04:52 -0300

    feat: add tempo medio de resposta e tempo insercao de processos

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class RoundRobin:
     def __init__(self, quantum):
         self.quantum = quantum
         self.processos = []
+        self.tempo_chegada_processos = []
         self.qtd_processos = 0
         self.tempo_duracao = 0
         self.tempo_atual = 0
@@ -22,7 +23,7 @@ class RoundRobin:
     def add_processo(self, nome, duracao, horario_chegada):
         processo = Processo(nome, duracao)
         processo.tempo_chegada = horario_chegada
-        
+        self.tempo_chegada_processos.append(horario_chegada)
         self.processos.append(processo)
         self.qtd_processos += 1
         self.tempo_duracao += duracao
@@ -37,14 +38,16 @@ class RoundRobin:
                 processo.inicio = self.tempo_atual
                 processo.tempoRes = self.tempo_atual
             while tempo_ate_quantum <= self.quantum and processo.tempo < processo.duracao:
+                while self.tempo_chegada_processos and self.tempo_chegada_processos[0] <= self.tempo_atual:
+                    print(f"Novo processo inserido no tempo {self.tempo_atual}.")
+                    self.tempo_chegada_processos.pop(0)
                 processo.tempo += 1
                 self.tempo_atual += 1
                 tempo_ate_quantum += 1
-                # print(f"Processo {processo.nome} executando. Tempo atual: {self.tempo_atual}. Tempo de execução: {processo.duracao}")
+                print(f"Processo {processo.nome} executando. Tempo atual: {self.tempo_atual}. Tempo de execução: {processo.duracao}. Tempo de execucao restante: {processo.duracao - processo.tempo}. Tempo de chegada: {processo.tempo_chegada}")
             tempo_ate_quantum = 1
             if processo.tempo >= processo.duracao:
                 processo.fim = True
-                # print(f"Processo {processo.nome} finalizado. {self.tempo_atual} unidades de tempo. {processo.tempo_chegada}")
                 processo.tempo_finalizacao = self.tempo_atual
                 processo.tempo_retorno = processo.tempo_finalizacao - processo.tempo_chegada
                 processo.tempo_resposta = processo.inicio - processo.tempo_chegada
@@ -55,16 +58,18 @@ class RoundRobin:
         
     def listar_processos_finalizados(self, processos_finalizados):
         processos_finalizados.sort(key=lambda x: x.nome)
+        tempo_medio_retorno = sum(p.tempo_resposta for p in processos_finalizados) / len(processos_finalizados)
         print("Processos Finalizados:")
         print("-" * 20)
         for process in processos_finalizados:
             self.listar_dados(process)
+        print(f"Tempo medio de resposta: {tempo_medio_retorno:.2f}")
         print("-" * 20)
     def listar_dados(self, process: Processo):
         
         print(f"Processo: {process.nome}")
         print(f"Chegada: {process.tempo_chegada}")
-        print(f"Começa em: {process.inicio}")
+        print(f"Comeca em: {process.inicio}")
         print(f"Tempo de Execução: {process.duracao}")
         print(f"Tempo de Fim: {process.tempo_finalizacao}")
         print(f"Tempo de Resposta: {process.tempo_resposta}")
@@ -84,4 +89,5 @@ round_robin_2 = RoundRobin(2)
 round_robin_2.add_processo("P1", 5, 0)                   
 round_robin_2.add_processo("P2", 3, 1)                   
 round_robin_2.add_processo("P3", 6, 2)      
-round_robin_2.executar()             
+round_robin_2.executar() 
+                      
